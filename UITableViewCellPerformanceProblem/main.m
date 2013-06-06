47473f72d07cbc6a7ea3fee848f60526149b4c57
Anthony Dmitriyev <antstorm@gmail.com>
2013-06-06 16:24:59 +0300

    Initial Commit

---
+++ UITableViewCellPerformanceProblem/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewCellPerformanceProblem
+//
+//  Created by Anton Dmitriyev on 06.06.13.
+//  Copyright (c) 2013 ANTStorm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
